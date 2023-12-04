7fc71781c4919e774997f8af6c6c8e018ea3156a
Imed Cherfaoui <imadeddine300@hotmail.com>
2023-12-04 14:01:41 +0800

    test

--- index.js
+++ index.js
@@ -7,8 +7,34 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/search/:username", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.github.com/users/${req.params.username}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_API_KEY}`,
+          "Access-Control-Allow-Origin": "*", // Add this line
+        },
+      }
+    );
+
+    const userData = {
+      name: response.data.name || response.data.login,
+      bio: response.data.bio || "",
+      avatarUrl: response.data.avatar_url,
+      followers: response.data.followers,
+      following: response.data.following,
+      publicRepos: response.data.public_repos,
+    };
+
+    res.json(userData);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(error.response ? error.response.status : 500)
+      .json({ error: "User not found" });
+  }
 });
 
 app.get("/script.js", (req, res) => {
